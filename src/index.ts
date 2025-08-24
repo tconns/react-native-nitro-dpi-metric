7015710021b057327a25dd5b057f01decd32e0ba
tconns94 <thanhcongns94@gmail.com>
2025-08-25 00:49:55 +0700

    docs: ✏️ update docs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { NitroModules } from 'react-native-nitro-modules'
 import type { NitroDpiMetric } from './specs/NitroDpiMetric.nitro'
+import { PixelRatio } from 'react-native'
 
 const NitroDpiMetricHybridObject =
   NitroModules.createHybridObject<NitroDpiMetric>('NitroDpiMetric')
@@ -18,4 +19,8 @@ export function deviceInch(): number {
 
 export function getDpi(): number {
   return NitroDpiMetricHybridObject.getDpi()
+}
+
+export const cmToPx = (cm: number): number => {
+  return (cm * getDpi()) / PixelRatio.get() / 2.54
 }
